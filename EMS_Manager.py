53efb9ebacf34f3c68e5c08055780a0e1bb962db
Jana <janasamehacc@gmail.com>
2025-08-30 13:51:11 +0300

    Refactor EMS_Manager.py to use pyodbc for SQL Server connection and update Managers table creation

--- EMS_Manager.py
+++ EMS_Manager.py
@@ -1,10 +1,21 @@
-import  sqlite3 
-connection = sqlite3.connect('EMS.db')
-cursor = connection.cursor()
-cursor.execute('''CREATE TABLE IF NOT EXISTS Managers (
-                    id INTEGER PRIMARY KEY AUTOINCREMENT,
-                    name TEXT NOT NULL,
-                    department TEXT NOT NULL
-                )''')
-connection.commit()
-connection.close()
+import pyodbc
+
+# connection string
+conn = pyodbc.connect(
+    "Driver={ODBC Driver 18 for SQL Server};"
+    r"Server=.\SQLEXPRESS;"   
+    "Database=EMS;"
+    "Trusted_Connection=yes;"
+    "TrustServerCertificate=yes;"
+)
+cursor = conn.cursor()
+cursor.execute('''
+CREATE TABLE Managers (
+    id INT PRIMARY KEY IDENTITY(1,1),
+    name NVARCHAR(100) NOT NULL,
+    department NVARCHAR(100) NOT NULL
+)
+''')
+
+conn.commit()
+conn.close()
