90fc7ccae88f985daafbc6cf0de898ea55c92a62
Jana <janasamehacc@gmail.com>
2025-08-30 13:59:08 +0300

    Add EMS_Employee.py to create Employees table in SQL Server database

---
+++ EMS_Employee.py
@@ -0,0 +1,21 @@
+import pyodbc 
+conn= pyodbc.connect( driver = '{ODBC Driver 18 for SQL Server};',
+                     server=r'.\SQLEXPRESS;',
+                     database='EMS;',
+                     trusted_connection='yes;',
+                        TrustServerCertificate='yes;'
+
+)
+cursor=conn.cursor()
+cursor.execute('''
+IF OBJECT_ID('Employees', 'U') IS NULL
+CREATE TABLE Employees (
+    id INT PRIMARY KEY IDENTITY(1,1),
+    name NVARCHAR(100) NOT NULL,
+    department NVARCHAR(100) NOT NULL,
+    salary INT NOT NULL
+)
+''')
+
+conn.commit()
+conn.close()
