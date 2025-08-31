7d1cf25fc9e206ed6f884f2c6bd0336b3a68991f
Jana <janasamehacc@gmail.com>
2025-08-31 11:52:17 +0300

    changed john salary

--- EMS_Employee.py
+++ EMS_Employee.py
@@ -16,12 +16,17 @@ cursor = conn.cursor()
 # ''')
 # cursor.execute(''' SELECT * FROM Employees JOIN Managers ON Employees.manager_id = Managers.id ''')
 
-cursor.execute(''' INSERT INTO Employees ( name,department, salary, manager_id) 
-                VALUES ( 'John Jay', 'IT', 60000, 1), 
-                ( 'Jane Doe', 'HR', 55000, 2),
-                ( 'Mike Smith', 'Finance', 70000, 1),
-                ( 'Emily Davis', 'IT', 65000, 1),
-                ('Sarah Johnson', 'Marketing', 60000, 3) 
-                ''')
+# cursor.execute(''' INSERT INTO Employees ( name,department, salary, manager_id) 
+#                 VALUES ( 'John Jay', 'IT', 60000, 1), 
+#                 ( 'Jane Doe', 'HR', 55000, 2),
+#                 ( 'Mike Smith', 'Finance', 70000, 1),
+#                 ( 'Emily Davis', 'IT', 65000, 1),
+#                 ('Sarah Johnson', 'Marketing', 60000, 3) 
+#                 ''')
+# cursor.execute(''' SELECT * FROM Employees ''')
+cursor.execute(''' UPDATE Employees
+SET salary = 1000
+WHERE name = 'John Jay';
+ ''')
 conn.commit() 
 conn.close()
